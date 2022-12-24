f3159743758d75ee8bc727b1efa63ae09475af01
Elena <zagre8elena@yandex.ru>
2022-12-24 13:57:59 +0300

    change Task4

--- Lesson4/Task4.py
+++ Lesson4/Task4.py
@@ -26,18 +26,21 @@ def creat_sps(k):
 def creat_str(lst):
     wrt = ''
     n = len(lst)
-    for i in range(n):
-        if i != n - 1 and i != n-2 and lst[i] != 0:
-            wrt += f'{lst[i]}x^{n-i-1}'
-            wrt += ' + '
-        elif i == n - 2 and lst[i] != 0:
-            wrt += f'{lst[i]}x'
-            if lst[i+1] != 0:
+    if n < 2:
+        wrt = 'x = 0'
+    else:
+        for i in range(n):
+            if i != n - 1 and i != n-2 and lst[i] != 0:
+                wrt += f'{lst[i]}x^{n-i-1}'
                 wrt += ' + '
-        elif i == n - 1 and lst[i] != 0:
-            wrt += f'{lst[i]} = 0'
-        elif i == n - 1 and lst[i] == 0:
-            wrt += ' = 0'
+            elif i == n - 2 and lst[i] != 0:
+                wrt += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wrt += ' + '
+            elif i == n - 1 and lst[i] != 0:
+                wrt += f'{lst[i]} = 0'
+            elif i == n - 1 and lst[i] == 0:
+                wrt += ' = 0'
     return wrt
 k = randint(2,10)
 for i in range(3):
