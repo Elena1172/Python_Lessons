2950fc8d3edabeb338c9d42f8b50ea741e2ad179
Elena <zagre8elena@yandex.ru>
2023-01-22 15:11:08 +0300

    added exep

---
+++ Lesson7/exep.py
@@ -0,0 +1,31 @@
+def get_numder():
+    while True:
+        try:
+            n = float(input('Enter number: '))
+            return n
+        except ValueError:
+            print('Error ')
+            logging.warning('ValueError')
+def get_numder1():
+    while True:
+        try:
+            n = float(input('Enter number: '))
+            100/n
+            return n
+        except Exception:
+            print('ValueError')
+            logging.warning('ValueError')
+        except ZeroDivisionError:
+            print('ZeroDivisionError')
+            logging.warning('ZeroDivisionError')
+def get_numder2():
+    while True:
+        try:
+            n = get_numder()
+            m = get_numder()
+            nm = complex(n,m)
+            1/nm
+            return nm
+        except ZeroDivisionError:
+            print('ZeroDivisionError')
+            logging.warning('ZeroDivisionError')
