e1dc9f6a170a8aa40c8802ecca4e196e33c16cfe
Elena <zagre8elena@yandex.ru>
2023-01-22 15:22:31 +0300

    added mod_calc

---
+++ Lesson7/mod_calc.py
@@ -0,0 +1,17 @@
+def sum(a,b):
+    return(a + b)
+def sub(a,b):
+    return(a - b)
+def mul(a,b):
+    return(a * b)
+def div01(a,b):
+    return(a / b)
+def div02(a,b):
+    return(a // b)
+def div03(a,b):
+    return(a % b)
+def pow(a,b):
+    return(a ** b)
+def sqrt(a):
+    return(a ** 0.5)
+        
