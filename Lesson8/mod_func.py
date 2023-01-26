dea40a19266fa069babfe2af7a7f5c4b162dcbd5
Elena <zagre8elena@yandex.ru>
2023-01-26 15:17:41 +0300

    change file

--- Lesson8/mod_func.py
+++ Lesson8/mod_func.py
@@ -66,3 +66,131 @@ def del_data2():
     with s.connect('base.db') as bd:
         cur = bd.cursor()
         cur.execute('DELETE FROM клиенты WHERE ID == ?',(n1,))
+def upd_data():
+     while True:
+        bd = s.connect('base.db')
+        cur = bd.cursor()
+        n2 = input('Введите для изменения данных \n'
+                        'в столбце "дата"  1\n'
+                        'в столбце "менеджер" 2\n'
+                        'в столбце "клиент" 3\n'
+                        'в столбце "товар" 4\n'
+                        'в столбце "сумма" 5\n'
+                        'для отмены 0\n')
+        
+        match n2:
+                case '1':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE заказы SET дата ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '2':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE заказы SET менеджер ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '3':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE заказы SET клиент ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '4':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE заказы SET товар ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '5':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE заказы SET сумма ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '0':
+                    break
+                case _:
+                    print('Ошибка ввода')
+                    break
+def upd_data1():
+    while True:
+        bd = s.connect('base.db')
+        cur = bd.cursor()
+        n2 = input('Введите для изменения данных \n'
+                        'в столбце "фамилия" 1\n'
+                        'в столбце "имя" 2\n'
+                        'в столбце "дата рождения" 3\n'
+                        'в столбце "должность" 4\n'
+                        'для отмены 0\n')
+        match n2:
+                case '1':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE сотрудники SET фамилия ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '2':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE сотрудники SET имя ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '3':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE сотрудники SET дата_рождения ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '4':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE сотрудники SET должность ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '0':
+                    break
+                case _:
+                    print('Ошибка ввода')
+                    break
+def upd_data2():
+    while True:
+        bd = s.connect('base.db')
+        cur = bd.cursor()
+        n2 = input('ВВедите для изменения данных \n'
+                        'в столбце "фамилия"  1\n'
+                        'в столбце "имя" 2\n'
+                        'в столбце "телефон" 3\n'
+                        'в столбце "e_mail" 4\n'
+                        'для отмены 0\n')
+        match n2:
+                case '1':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE клиенты SET фамилия ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '2':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE клиенты SET имя ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '3':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE клиенты SET телефон ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '4':
+                    n1 = input('Введите ID строки для изменения ')
+                    nn = input('Новое значение  ')
+                    cur.execute('UPDATE клиенты SET e_mail ==? WHERE ID == ?',(nn,n1))
+                    bd.commit()
+                    bd.close()
+                case '0':
+                    break
+                case _:
+                    print('Ошибка ввода')
+                    break
